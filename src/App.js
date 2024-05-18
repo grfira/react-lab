cbc993a4d044b5ce0cce4de94c9c37a71a0be7f8
grfira <gm_c@wp.pl>
2024-05-18 14:14:12 +0200

    add useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'login@student.agh.edu.pl';
+    // let email = 'login@student.agh.edu.pl';
+    //const [a,b,c]= [1,2,3];
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
+
     function handleChange(event) {
-
-        console.log(event.target.value);
-
+        setEmail(event.target.value)
+        //console.log(event.target.value);
     }
+
+    let header2 = <h3>MAsz krotki adres</h3>
+    if (email.length < 6) {
+        header2 = <h3> Msz krótki adres!"</h3>
+    } else {
+        if (email.length > 20) {
+            header2 = <h3> "Masz długi adres!"</h3>
+        } else header2 = <h3> "W sam raz"</h3>
+    }
+
+
     return (
         <div>
-
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email.toLocaleUpperCase()}</h2>
+            {header2}
             <input type="text" onChange={handleChange}/>
 
         </div>
